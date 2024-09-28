620cab72dd59b388e0389940f9d916bc996fe885
unknown <wallaceting29@gmail.com>
2024-09-28 17:32:58 +0800

    FUCK U EVENT LISTENER

--- Assets/_Scripts/Shop/ShopSystem.cs
+++ Assets/_Scripts/Shop/ShopSystem.cs
@@ -20,7 +20,7 @@ public class ShopSystem : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            // DontDestroyOnLoad(gameObject);
         }
         else
         {
